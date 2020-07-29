c533c9efbb434c842c728bba115512ea2d64fb02
Archit Adhikari <architadhikary@gmail.com>
2020-07-29 13:25:24 +0545

    inialized git repo for frontend mentor projects; include one project- article preview component

---
+++ article preview component/app.js
@@ -0,0 +1,15 @@
+const shareBtn = document.querySelector(".content__icon");
+const shareDiv = document.querySelector(".share");
+
+// click share event
+
+shareBtn.addEventListener("click", shareDialog);
+
+function shareDialog() {
+  //   shareDiv.style.display = "flex";
+  if (shareDiv.style.display === "none") {
+    shareDiv.style.display = "flex";
+  } else {
+    shareDiv.style.display = "none";
+  }
+}
