f964430b9f0c3d4d53c2f2f35ade7bbd8cd5da58
Archit <architadhikary@gmail.com>
2021-06-26 00:00:51 +0545

    new changes

---
+++ todo-app-main (Intermediate)/dist/app.js
@@ -0,0 +1,23 @@
+//define UI variables
+const toggle = document.getElementById("toggle");
+const form = document.querySelector(".input-field");
+const todoInput = document.getElementById("todo");
+
+//events
+toggle.addEventListener("change", () => {
+  console.log("clicked");
+  document.body.classList.toggle("dark");
+});
+
+form.addEventListener("submit", addTask);
+
+//Add task
+function addTask(e) {
+  console.log(todoInput.value);
+console.log('dipti');
+  // if(todoInput.value !=== ''){
+
+  // }
+  //random comment
+  e.preventDefault();
+}
